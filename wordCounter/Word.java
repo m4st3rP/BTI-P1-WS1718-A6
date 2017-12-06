7f5d756fbc2e3deeb3d4c92afdb725bf94742866
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-12-06 19:57:22 +0100

    Update Word.java

--- wordCounter/Word.java
+++ wordCounter/Word.java
@@ -1,25 +1,25 @@
 package wordCounter;
 
-public class Word implements Comparable{
+public class Word implements Comparable<Object> {
     private final String word;
 
-    
+
     public Word(String word) {
         this.word = word;
     }
-    
-    
-    
+
+
+
     public String getWord() {
         return word;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         Word otherWord = (Word) obj;
         return (this.word.toLowerCase().equals(otherWord.word.toLowerCase()));
     }
-    
+
     @Override
     public int hashCode() {
         return 1;
