af4eb413a4a7a1e818d1c3f820676d4d8b049fce
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-12-09 14:23:23 +0100

    Update CD.java

--- media/CD.java
+++ media/CD.java
@@ -10,17 +10,15 @@ public class CD extends Disc {
     
     @Override
     public String toString() {
-        return "Class: " + this.getClass() + ", Title: " + title + ", Content Type: " + contentType + ", Interpret: " + interpret;
-    }
+        return String.format("[<%s>: Interpret=%s %s]", CD.class.getSimpleName(), interpret, super.toString());
+}
     
-//    @Override
-//    public boolean equals(Object obj) {
-//        CD objcet = (CD) obj;
-//        if (this.title == objcet.title && this.contentType == objcet.contentType && this.interpret == objcet.interpret) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
+    @Override
+    public boolean equals(Object other) {
+        boolean equal = super.equals(other);
+        if (getClass() != other.getClass())
+            return false;
+        return ((CD) other).interpret.equals(this.interpret) && equal;
+}
 }
 
