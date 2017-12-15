20e736f020480a741331b21e55d1844a1fe85d56
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-12-15 12:37:21 +0100

    Update MyList.java

--- list/MyList.java
+++ list/MyList.java
@@ -1,247 +1,301 @@
 package list;
 
 public class MyList<T> {
-    // Attributes
-    private Node<T> head;
-    private Node<T> tail;
-    private int size;
 
-    // Constructor
-    public MyList() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
+	private Node<T> first;
+	private Node<T> last;
+	private int size;
 
-    // Methods
-    public T getNo(int position) {
-        assert -1 < position && position < getSize() : "Unsupported Argument";
-        return iGetNodeNo(position).info;
-    }
+	public MyList() {
+		first = null;
+		last = null;
+		size = 0;
+	}
 
-    public T getF() {
-        return head.info;
-    }
+	/**
+	 * 
+	 * @param position the index of the element in the list to return
+	 * @return data of element at position
+	 */
+	public T getNo(int position) {
+		assert 0 <= position && position < getSize() : "Argument no is not in size";
+		return iGetNodeNo(position).getData();
+	}
 
-    public T getL() {
-        return tail.info;
-    }
+	/**
+	 * 
+	 * @return the first element's data of the list. List shouldn't be empty.
+	 */
+	public T getF() {
+	    assert size > 0 : "List is empty";
+		return first.getData();
+	}
 
-    public T extractNo(int position) {
-        assert -1 < position && position < getSize() : "Unsupported Argument";
-        T info = iGetNodeNo(position).info;
-        iRemoveNode(iGetNodeNo(position));
-        return info;
-    }
+	/**
+	 * 
+	 * @return the last element's data of the list. List shouldn't be empty.
+	 */
+	public T getL() {
+	    assert size > 0 : "List is empty";
+		return last.getData();
+	}
 
-    public T extractF() {
-        if (!isEmpty()) {
-            T oldHead = head.info;
-            iRemoveNode(head);
-            return oldHead;
-        } else {
-            return null;
-        }
-    }
+	/**
+	 * remove a specific element from the list
+	 * @param position the position of the element which should be removed
+	 * @return the removed element data
+	 */
+	public T extractNo(int position) {
+		assert 0 <= position && position < getSize() : "Argument no is not in size";
+		Node<T> nodeToExtract = iGetNodeNo(position);
+		iRemoveNode(nodeToExtract);
+		return nodeToExtract.getData();
+	}
 
-    public T extractL() {
-        if (!isEmpty()) {
-            T oldTail = tail.info;
-            iRemoveNode(tail);
-            return oldTail;
-        } else {
-            return null;
-        }
-    }
+	/**
+	 * remove the first element of the list and return it. 
+	 * @return the first element data of the list
+	 */
+	public T extractF() {
+		if (!isEmpty()) {
+			T ret = first.getData();
+			iRemoveNode(first);
+			return ret;
+		} else {
+			return null;
+		}
+	}
 
-    public boolean putNo(int no, T element) {
-        assert 0 <= no && no <= getSize() : "Argument no is not in size";
-        assert element != null : "Argument can't be null!";
+	/**
+	 * remove the last element of the list and return it
+	 * @return the last element data of the list
+	 */
+	public T extractL() {
+		if (!isEmpty()) {
+			T ret = last.getData();
+			iRemoveNode(last);
+			return ret;
+		} else {
+			return null;
+		}
+	}
 
-        Node<T> newNode = new Node<>(element);
-        Node<T> node = iGetNodeNo(no);
+	/**
+	 * add a new element to the list at a specific position in the list or at the end of the list. 
+	 * @param position position where the new element should be added. Needs to be 0 <= position <= size
+	 * @param element the element that should be added
+	 * @return
+	 */
+	public boolean putNo(int position, T element) {
+		assert 0 <= position && position <= getSize() : "Argument no is not in size";
+		assert element != null : "Argument can't be null!";
 
-        if (no > size)
-            return false;
+		Node<T> newNode = new Node<>(element);
+		Node<T> node = iGetNodeNo(position);
 
-        if (size == 0) {
-            head = newNode;
-            tail = head;
-        } else if (no == 0) {
-            node.previous = newNode;
-            newNode.next = node;
-            head = newNode;
-        } else if (no == size) {
-            newNode.previous = tail;
-            tail.next = newNode;
-            tail = newNode;
-        } else {
-            node.previous.next = newNode;
-            newNode.previous = node.previous;
-            node.previous = newNode;
-            newNode.next = node;
-        }
+		if (position > size)
+			return false;
 
-        size++;
-        return true;
-    }
+		if (isEmpty()) {
+			first = newNode;
+			last = first;
+		} else if (position == 0) { //need to set new first element
+			node.previous = newNode;
+			newNode.next = node;
+			first = newNode;
+		} else if (position == size) {	//need to set new last element
+			newNode.previous = last;
+			last.next = newNode;
+			last = newNode;
+		} else {	//usual case, don't get the order of new assignments wrong here 
+			node.previous.next = newNode;
+			newNode.previous = node.previous;
+			node.previous = newNode;
+			newNode.next = node;
+		}
 
-    public void putF(T element) {
-        Node<T> headNode = head;
-        if (!isEmpty()) {
-            Node<T> newheadNode = new Node<>(element);
-            headNode.previous = newheadNode;
-            newheadNode.next = headNode;
-            head = newheadNode;
-        } else {
-            head = new Node<>(element);
-            tail = head;
-        }
+		size++;
+		return true;
+	}
 
-        size++;
-    }
+	/**
+	 * add a new element to the begin of the list. The other elements are shifted back
+	 * @param element the element that should be added
+	 */
+	public void putF(T element) {
+		Node<T> firstNode = first;
+		if (!isEmpty()) {
+			Node<T> newFirstNode = new Node<>(element);
+			firstNode.previous = newFirstNode;
+			newFirstNode.next = firstNode;
+			first = newFirstNode;
+		} else {
+			first = new Node<>(element);
+			last = first;
+		}
 
-    public void putL(T element) {
-        Node<T> tailNode = tail;
-        if (!isEmpty()) {
-            Node<T> newtailNode = new Node<>(element);
-            tailNode.next = newtailNode;
-            newtailNode.previous = tailNode;
-            tail = newtailNode;
-        } else {
-            tail = new Node<>(element);
-            head = tail;
-        }
+		size++;
+	}
 
-        size++;
-    }
+	/**
+	 * add a new element to the end of the list
+	 * @param element the element that should be added
+	 */
+	public void putL(T element) {
+		Node<T> lastNode = last;
+		if (!isEmpty()) {
+			Node<T> newLastNode = new Node<>(element);
+			lastNode.next = newLastNode;
+			newLastNode.previous = lastNode;
+			last = newLastNode;
+		} else {
+			last = new Node<>(element);
+			first = last;
+		}
 
-    public T setNo(int position, T info) {
-        assert -1 < position && position <= getSize() && info != null : "Unsupported Argument";
+		size++;
+	}
 
-        T oldInfo = null;
-        if (getSize() != position) {
-            oldInfo = iGetNodeNo(position).info;
-            iGetNodeNo(position).info = info;
-        } else {
-            putL(info);
-        }
+	/**
+	 * write new element-object into the list at a specific position and replace the element at this position.
+	 * @param position position where the element should be written to with 0 <= pos < size
+	 * @param element the element that should be written into the list
+	 * @return the overwritten/replaced element's data
+	 */
+	public T setNo(int position, T element) {
+		assert 0 <= position && position < getSize() : "Argument no is not in size";
+		assert element != null : "Argument can't be null!";
+		Node<T> node = iGetNodeNo(position);
 
-        return oldInfo;
-    }
+		Node<T> newNode = new Node<>(element);
+		newNode.next = node.next;
 
-    public void removeNo(int position) {
-        iRemoveNode(iGetNodeNo(position));
-    }
+		if (node == first) {
+			if (first.hasNext())
+				first.next.previous = newNode;
+			newNode.next = first.next;
+			first = newNode;
+		} else {
+			newNode.previous = node.previous;
+			node.previous.next = newNode;
+			if (node.hasNext())
+				node.next.previous = newNode;
+		}
 
-    public boolean remove(T info) {
-        return iRemoveNode(iSearchNode(info));
-    }
+		return node.getData();
+	}
 
-    public void clear() {
-        size = 0;
-        head = null;
-        tail = null;
-    }
+	/**
+	 * @param position position of the element which should be removed from the list
+	 */
+	public void removeNo(int position) {
+	    assert 0 <= position && position < size : "Illegal Argument";
+		iRemoveNode(iGetNodeNo(position));
+	}
 
-    public boolean isEmpty() {
-        if (head != null && tail != null) {
-            return false;
-        } else if (head == null && tail == null) {
-            return true;
-        } else {
-            System.out.println("Serious error: Either head or tail are null but not both!");
-            return false;
-        }
-    }
+	/**
+	 * removes first occurance of a specific element in the list
+	 * @param element the element which should be removed from the list
+	 * @return true if the element was removed, false if the element wasn't in the list
+	 */
+	public boolean remove(T element) {
+		return iRemoveNode(iSearchNode(element));
+	}
 
-    public int getSize() {
-        return size;
-    }
+	/**
+	 * clear the list and set the size to zero
+	 */
+	public void clear() {
+		size = 0;
+		first = null;
+		last = null;
+	}
 
-    public boolean contains(T info) {
-        return iSearchNode(info) != null;
-    }
+	/**
+	 * 
+	 * @return true if the list is empty, false if not
+	 */
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
-    // Helping Methods
-    protected Node<T> iSearchNode(T info) {
-        if (info == null)
-            return null;
+	/**
+	 * 
+	 * @return the size of the list
+	 */
+	public int getSize() {
+		return size;
+	}
 
-        Node<T> work = head;
-        while (work != null && !work.info.equals(info)) {
-            work = work.next;
-        }
-        return work;
-    }
+	/**
+	 * 
+	 * @param element
+	 *            of type T to check
+	 * @return true if element is in the list, false if not
+	 */
+	public boolean contains(T element) {
+		assert element != null : "Argument can't be null!";
+		return iSearchNode(element) != null;
+	}
 
-    protected Node<T> iGetNodeNo(int position) {
-        Node<T> work = head;
+	// -----------------------------------------------------------------------------------------
 
-        if (work == null || size < position)
-            return null;
+	/**
+	 * private helper methods
+	 */
 
-        for (int i = 0; i < position; i++) {
-            work = work.next;
-        }
+	private Node<T> iGetNodeNo(int position) {
+		// assert 0 <= no && no < getSize() : "Argument no is not in size";
+		Node<T> node = first;
+		if (node == null || position >= size)
+			return null;
 
-        return work;
-    }
+		for (int i = 0; i < position; i++) {
+			node = node.next;
+		}
 
-    protected boolean iRemoveNode(Node<T> node) {
+		return node;
+	}
 
-        if (node == null)
-            return false;
+	private Node<T> iSearchNode(T element) {
+		assert element != null : "Argument can't be null!";
+		Node<T> node = first;
+		while (node != null) {
+			if (node.getData().equals(element))
+				return node;
+			node = node.next;
+		}
+		return node;
+	}
 
-        if (!node.equals(head) && !node.equals(tail)) {
-            node.previous.next = node.next;
-            node.next.previous = node.previous;
-            size--;
-            return true;
-        } else if (node.equals(head) && !node.equals(tail)) {
-            head = node.next;
-            head.previous = null;
-            size--;
-            return true;
-        } else if (!node.equals(head) && node.equals(tail)) {
-            tail.next = null;
-            tail = node.previous;
-            size--;
-            return true;
-        } else if (node.equals(head) && node.equals(tail)) {
-            clear();
-            return true;
-        } else {
-            return false;
-        }
-    }
+	private boolean iRemoveNode(Node<T> node) {
+		boolean removed = false;
 
+		if (node == null)
+			return removed;
 
-    public void printElemF2L() {
-        for (int i = 0; i < getSize(); i++) {
-            System.out.println(getNo(i));
-        }
-        System.out.println("Size: " + getSize());
-    }
+		if (node != first && node != last) {
+			node.previous.next = node.next;
+			node.next.previous = node.previous;
+			removed = true;
+		}
+		if (node == first) {
+			first = node.next;
+			if (first != null)
+				first.previous = null;
+			removed = true;
+		}
+		if (node == last) {
+			last.next = null;
+			last = node.previous;
+			removed = true;
+		}
 
-    public void printElemL2F() {
-        for (int i = getSize() - 1; i > -1; i--) {
-            System.out.println(getNo(i));
-        }
-        System.out.println("Size: " + getSize());
-    }
+		if (removed)
+			size--;
 
-    protected void printNodeF2L() {
-        for (int i = 0; i < getSize(); i++) {
-            System.out.println("Info: " + iGetNodeNo(i).info + " Previous: " + iGetNodeNo(i).previous + " Next: " + iGetNodeNo(i).next);
-        }
-        System.out.println("Size: " + getSize());
-    }
+		return removed;
+	}
 
-    protected void printNodeL2F() {
-        for (int i = getSize() - 1; i > -1; i--) {
-            System.out.println("Info: " + iGetNodeNo(i).info + " Previous: " + iGetNodeNo(i).previous + " Next: " + iGetNodeNo(i).next);
-        }
-        System.out.println("Size: " + getSize());
-    }
+	// -----------------------------------------------------------------------------------------
 }
